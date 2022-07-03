2c223b65c9e025baf0e36ac7df8abc61747ee769
Nathan Craddock <nzcraddock@gmail.com>
2022-07-03 10:02:46 -0600

    add zf.rb

---
+++ zf.rb
@@ -0,0 +1,21 @@
+class Zf < Formula
+  desc "a commandline fuzzy finder that prioritizes matches on filenames"
+  homepage "https://github.com/natecraddock/zf"
+  url "https://github.com/natecraddock/zf/archive/refs/tags/0.5.tar.gz"
+  sha256 "3b5ff72571867f22d51dd1e349c86bf4b00b90604ff3f26de1b95bf752523e8e"
+  license "MIT"
+  head "https://github.com/natecraddock/zf.git", branch: "master"
+  version "0.5"
+
+  depends_on "zig" => :build
+
+  def install
+    system "zig", "build", "-Drelease-fast=true"
+    man1.install "doc/zf.1"
+    bin.install "zig-out/bin/zf"
+  end
+
+  test do
+    assert_equal "zig", pipe_output("#{bin}/zf -f zg", "take\off\every\nzig").chomp
+  end
+end
