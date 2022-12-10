cf68ceed947d5161638cfaa0f63d9eef71f1d6eb
Nathan Craddock <nzcraddock@gmail.com>
2022-12-10 16:32:25 -0700

    Update zf to 0.6.0

--- zf.rb
+++ zf.rb
@@ -1,8 +1,8 @@
 class Zf < Formula
-  desc "a commandline fuzzy finder that prioritizes matches on filenames"
+  desc "command-line fuzzy finder that prioritizes matches on filenames"
   homepage "https://github.com/natecraddock/zf"
-  url "https://github.com/natecraddock/zf/archive/refs/tags/0.5.tar.gz"
-  sha256 "3b5ff72571867f22d51dd1e349c86bf4b00b90604ff3f26de1b95bf752523e8e"
+  url "https://github.com/natecraddock/zf/archive/refs/tags/0.6.0.tar.gz"
+  sha256 "9767ce1142933c753b749630229d10519c4c0b208f63931e43495acc632135e2"
   license "MIT"
   head "https://github.com/natecraddock/zf.git", branch: "master"
 
